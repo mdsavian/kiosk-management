782dce6400b810d3e57fe86f6fc70f17fbcf5ae4
mdsavian <marlon.savian@gmail.com>
2023-06-23 06:49:45 -0300

    feat(front): add react-router-dom and routes

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -2,12 +2,28 @@ import * as React from "react";
 import "./App.css";
 import KioskPage from "./pages/KioskPage";
 import SnackbarProvider from "./components/common/SnackBar/SnackbarProvider";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <KioskPage />,
+    },
+    {
+      path: "products",
+      element: <div>Add Products</div>,
+    },
+    {
+      path: "cart",
+      element: <div>Cart</div>,
+    },
+  ]);
+
   return (
     <div className="container">
       <SnackbarProvider>
-        <KioskPage />
+        <RouterProvider router={router} />
       </SnackbarProvider>
     </div>
   );

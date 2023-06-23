7226e1efb7010ba0f4175c9b8dc86e6bb3806ab5
mdsavian <marlon.savian@gmail.com>
2023-06-23 10:19:53 -0300

    feat(front): add products

---
+++ apps/frontend/src/assets/products/products.ts
@@ -0,0 +1,25 @@
+import coca from "./images/coca.jpeg";
+import fruki from "./images/fruki.jpeg";
+import guarana from "./images/guarana.jpeg";
+import laranja from "./images/laranja.webp";
+import leite from "./images/leite.webp";
+import maca from "./images/maca.webp";
+import mumu from "./images/mumu.webp";
+import pao from "./images/pao-leite.png";
+import pepsi from "./images/pepsi.jpeg";
+import sukita from "./images/sukita.webp";
+
+const products = [
+  { id: "A1", description: "Coca Cola", price: 7.25, image: coca },
+  { id: "A5", description: "Fruki", price: 4.5, image: fruki },
+  { id: "A3", description: "Guaraná Antarctica", price: 6.15, image: guarana },
+  { id: "A10", description: "Laranja", price: 6.5, image: laranja },
+  { id: "A8", description: "Leite", price: 6.1, image: leite },
+  { id: "A9", description: "Maçã", price: 4.15, image: maca },
+  { id: "A6", description: "Doce de Leite", price: 7.35, image: mumu },
+  { id: "A7", description: "Pão de Leite", price: 4.75, image: pao },
+  { id: "A2", description: "Pepsi", price: 7.25, image: pepsi },
+  { id: "A4", description: "Sukita", price: 5.25, image: sukita },
+];
+
+export default products;
--- apps/frontend/src/pages/ProductsPage.tsx
+++ apps/frontend/src/pages/ProductsPage.tsx
@@ -1,7 +1,30 @@
 import * as React from "react";
+import products from "../assets/products/products";
 
 const ProductsPage: React.FC = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <button className="rounded-full bg-emerald-500 p-2 mt-5">Go To cart</button>
+      <div className="grid grid-cols-3 gap-4">
+        {products.map((product) => (
+          <div className="border border-black p-2 ">
+            <img className="object-contain h-56 w-96" src={product.image}></img>
+
+            <h3 className="mt-2 text-center">{product.description}</h3>
+            <h4 className="mt-2 text-center">R$ {product.price}</h4>
+
+            <input
+              type="number"
+              className="w-full text-center mt-2 border"
+              placeholder="Quantity"
+            />
+
+            <button className="rounded-full bg-emerald-300 p-2 mt-3">Add to cart</button>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default ProductsPage;
